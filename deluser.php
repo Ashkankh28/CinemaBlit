92dd091d71562c7572d97de19c56e924022fb56d
Ashkankh28 <ask.Khodabakhsh@gmail.com>
2026-09-03 15:37:08 +0330

    Update deluser.php

--- deluser.php
+++ deluser.php
@@ -11,6 +11,35 @@ require_once "config.php";
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
+    $stmt = mysqli_prepare($link,
+    "UPDATE seats s INNER JOIN ticket t ON s.tickid = t.tickid 
+     SET s.reserved = 0, s.tickid = 0
+     WHERE t.userid = ?");
+    
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    $restoreStmt = mysqli_prepare($link,
+        "UPDATE movies m
+        INNER JOIN (
+            SELECT movid, SUM(tickcount) AS totalTickets
+            FROM ticket
+            WHERE userid = ?
+            GROUP BY movid
+        ) t ON m.movid = t.movid
+        SET m.tickets = m.tickets + t.totalTickets");
+
+    mysqli_stmt_bind_param($restoreStmt, "i", $id);
+    mysqli_stmt_execute($restoreStmt);
+    mysqli_stmt_close($restoreStmt);
+
+    $stmt = mysqli_prepare($link, "DELETE FROM ticket WHERE userid = ?");
+
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
     $stmt = mysqli_prepare($link, "DELETE FROM users WHERE id = ?");
 
     mysqli_stmt_bind_param($stmt, "i", $id);
